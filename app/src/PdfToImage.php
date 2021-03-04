cdfb0d45ea1a4742f2bf7cf69c10abbcc7b2a81d
Lucas <ti12@viacometa.com.br>
2021-03-04 10:35:39 -0400

    fix: exclui file tmp antes de criar novo

--- app/src/PdfToImage.php
+++ app/src/PdfToImage.php
@@ -3,7 +3,8 @@
 namespace Lucasmota\Src;
 
 
-class PdfToImage {
+class PdfToImage
+{
     const  TMP = __DIR__ . "/tmp/tmp.tiff";
 
     public function __construct(string $filename)
@@ -13,15 +14,17 @@ class PdfToImage {
         $this->filename = $filename;
     }
 
-    public function tiff(){
-      exec("convert -quality 100 -density 300 -depth 8 -strip -background white -alpha off {$this->filename}[0] ". self::TMP , $this->outputExec);
-      return self::TMP;
+
+    public function tiff()
+    {
+        $this->destroy();
+        exec("convert -quality 100 -density 300 -depth 8 -strip -background white -alpha off {$this->filename}[0] " . self::TMP, $this->outputExec);
+        return self::TMP;
     }
 
     public function destroy()
     {
         // echo $filename;
-        if (!unlink($this->tmp)) throw new \Exception("Erro ao excluir o arquivo");
+        if (!unlink(self::TMP)) throw new \Exception("Erro ao excluir o arquivo");
     }
-
 }
